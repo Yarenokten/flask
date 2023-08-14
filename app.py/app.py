1b2659aa7263fd11dfdec9a032f93b16a01f5db4
yaren okten <yaren.yk28@gmail.com>
2023-08-14 13:12:09 +0300

    first commit

---
+++ app.py/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/yaren')
+def i_index():
+    user_name = "yaren"
+    return render_template('index.html', name=user_name)
+
+@app.route('/deneme')
+def ansayfa():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
