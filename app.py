f9065af082366cb854cb28ad0ec1fb5099243978
yarenokten <yaren.yk28@gmail.com>
2023-08-14 17:54:15 +0300

    degisiklik

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return render_template('deneme.html')
+    return render_template('index.html')
 
 if _name_ == "_main_":
     app.run(debug=True)
